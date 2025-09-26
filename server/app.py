0f1d6e5f51cf60408a18a16e3120285de872a772
Dorothy <dorothywanjiru32@gmail.com>
2025-09-26 12:56:20 +0300

    iam

--- server/app.py
+++ server/app.py
@@ -8,19 +8,75 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        try:
+            user = User(
+                username=data['username'],
+                image_url=data.get('image_url'),
+                bio=data.get('bio')
+            )
+            user.password_hash = data['password']
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        except IntegrityError:
+            return {'error': 'Username already exists'}, 422
+        except (ValueError, KeyError):
+            return {'error': "Missing required fields"}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {'error': 'Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(User.username == data['username']).first()
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'error': 'Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return '', 204
+        else:
+            return {'error': 'Unauthorized'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            recipes = Recipe.query.all()
+            return [r.to_dict() for r in recipes], 200
+        else:
+            return {'error': 'Unauthorized'}, 401
+
+    def post(self):
+        if session.get('user_id'):
+            data = request.get_json()
+            try:
+                recipe = Recipe(
+                    title=data['title'],
+                    instructions=data['instructions'],
+                    minutes_to_complete=data['minutes_to_complete'],
+                    user_id=session['user_id']
+                )
+                db.session.add(recipe)
+                db.session.commit()
+                return recipe.to_dict(), 201
+            except (ValueError, KeyError):
+                return {'errors': ["Missing required fields"]}, 422
+        else:
+            return {'error': 'Unauthorized'}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/config.py
+++ server/config.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
@@ -7,7 +8,10 @@ from sqlalchemy import MetaData
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+if os.environ.get('FLASK_ENV') == 'testing':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
